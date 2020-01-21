1045e64abca4ff56b8e192e864fe99b0d47c500a
HIGOR CARDOSO GALVAO <higor.cgalvao@senacsp.edu.br>
2020-01-21 15:17:27 -0300

    segundo commit

--- SncMusic/Banco.cs
+++ SncMusic/Banco.cs
@@ -10,14 +10,25 @@ namespace SncMusic
 {
     public static class Banco
     {
+        static MySqlConnection cn;
+
         public static MySqlCommand Abrir()
         {
-            MySqlConnection cn = new MySqlConnection(Properties.Settings.Default.StrConn);
+            cn = new MySqlConnection(Properties.Settings.Default.StrConn);
             cn.Open();
             MySqlCommand comm = new MySqlCommand();
             comm.Connection = cn;
             return comm;
-        }
-    }
 
+        }
+        public static void Fechar()
+        {
+            // se o estado da conexão for aberto então fechar conexão
+            if (cn.State == System.Data.ConnectionState.Open)
+                cn.Close();
+            // não é necessário utilizar chaves em comandos ou estruturas caso tenha apenas uma comando
+
+        }
+
+    }
 }
